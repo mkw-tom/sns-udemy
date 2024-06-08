246264ce00f40afb051eff8e8059de98a861aba6
Tomu Maekawa <tomu.m1107@icloud.com>
2024-06-08 15:58:42 +0900

    proxyのエンドポイントをhttpからhttpsに変更してみた

--- frontend/src/actionCalls.js
+++ frontend/src/actionCalls.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginCall = async(user, dispatch) => {
   dispatch({type: "LOGIN_START"});
   try {
-    const res = await axios.post("auth/login", user)
+    const res = await axios.post("/auth/login", user)
     dispatch({type: "LOGIN_SUCCESS", payload: res.data});
   } catch(err) {
     dispatch({type: "LOGIN_ERROR", payload: err});
