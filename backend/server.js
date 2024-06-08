eb277d98ebd6ea9f118545f86778b77066a6d59c
Tomu Maekawa <tomu.m1107@icloud.com>
2024-06-08 18:35:40 +0900

    corsの許可を各apiに設定した

--- backend/server.js
+++ backend/server.js
@@ -16,21 +16,21 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("DBと接続中..."))
   .catch((err) => console.log(err));
 
-const corsOption = {
-  origin: "https://sns-udemy-six.vercel.app",
-  methods: "GET, PUT, PATCH, POST, DELETE",
-  crendentials: true,
-  optionSuccessStatus: 204,
-}
+// const corsOption = {
+//   origin: "https://sns-udemy-six.vercel.app",
+//   methods: "GET, PUT, PATCH, POST, DELETE",
+//   crendentials: true,
+//   optionSuccessStatus: 204,
+// }
 
 //ミドルウェア
-app.use(cors(corsOption));
+// app.use(cors(corsOption));
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "public/images")))
-app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postsRoute);
-app.use("/api/upload", uploadRoute);
+app.use("/api/users", cors(), userRoute);
+app.use("/api/auth", cors(), authRoute);
+app.use("/api/posts", cors(), postsRoute);
+app.use("/api/upload", cors(), uploadRoute);
 
 
 app.listen(PORT, () => console.log("サーバーが起動しましました"));
