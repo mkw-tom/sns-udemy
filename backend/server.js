d816e63a3f20ee688e7341c97851704c08e86901
Tomu Maekawa <tomu.m1107@icloud.com>
2024-06-08 18:44:11 +0900

    cors設定の記述を少し変えてみた

--- backend/server.js
+++ backend/server.js
@@ -16,21 +16,21 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("DBと接続中..."))
   .catch((err) => console.log(err));
 
-// const corsOption = {
-//   origin: "https://sns-udemy-six.vercel.app",
-//   methods: "GET, PUT, PATCH, POST, DELETE",
-//   crendentials: true,
-//   optionSuccessStatus: 204,
-// }
+const corsOption = {
+  origin: "https://sns-udemy-six.vercel.app",
+  methods: ["POST", "PUT", "PATCH", "DELETE"],
+  crendentials: true,
+  optionSuccessStatus: 200,
+}
 
 //ミドルウェア
 // app.use(cors(corsOption));
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "public/images")))
-app.use("/api/users", cors(), userRoute);
-app.use("/api/auth", cors(), authRoute);
-app.use("/api/posts", cors(), postsRoute);
-app.use("/api/upload", cors(), uploadRoute);
+app.use("/api/users", cors(corsOption), userRoute);
+app.use("/api/auth", cors(corsOption), authRoute);
+app.use("/api/posts", cors(corsOption), postsRoute);
+app.use("/api/upload", cors(corsOption), uploadRoute);
 
 
 app.listen(PORT, () => console.log("サーバーが起動しましました"));
